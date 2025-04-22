944b7c46de8bab92ddf331c6488eb454b9e7d45c
Anshuman Pattnaik <anshuman.pattnaik524@gmail.com>
2025-04-23 01:47:40 +0530

    Update kafka_producer_client.py

--- kafka_manager/kafka_producer_client.py
+++ kafka_manager/kafka_producer_client.py
@@ -35,10 +35,6 @@ class KafkaProducerClient:
         self._bootstrap_servers = bootstrap_servers
         self._producer = None
 
-    @property
-    def producer(self):
-        return self._producer
-
     def start(self):
         """
         This method starts the Kafka producer and connects to the Kafka broker(s).
