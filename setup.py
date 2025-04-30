adc1df8aa3c35df69fe9bf4403011fb478f10209
Anshuman Pattnaik <anshuman.pattnaik524@gmail.com>
2025-05-01 04:16:45 +0530

    Renamed README.md -> README.rst

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import os
 from setuptools import setup, find_packages
 
-with open(os.path.join(os.path.dirname(__file__), 'README.md'), encoding='utf-8') as file:
+with open(os.path.join(os.path.dirname(__file__), 'README.rst'), encoding='utf-8') as file:
     long_description = file.read()
 
 version = {}
